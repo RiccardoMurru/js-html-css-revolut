059c8833d0fbd81870993012203db42a374fad07
Riccardo Murru <riccardo.mr@gmail.com>
2020-04-14 18:54:52 +0200

    add. dropdown menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,31 @@
 /**
  *  Ricreare la navbar di revolut
     Focus è rendere funzionali i menu dropdown all’interno della navigazione principale
- */
+*/
 
- 
+$(document).ready(function() {
+
+    var dropdownLink = $('.dropdown-link');
+    var dropdownMenu = dropdownLink.next('.dropdown-menu');
+
+
+    dropdownLink.mouseenter(function () {
+        $(this).next(dropdownMenu).show().addClass('active');
+    });
+
+    dropdownLink.mouseleave(function () {
+        dropdownMenu.hide();
+
+    });
+
+    /* non funziona */
+
+
+
+
+
+
+
+
+// end document ready
+});
